6ff955abc535deb1297a3c6a7682228db4537e5f
Di Giovanni Federica <digiovannifederica23@gmail.com>
2024-11-23 17:09:51 +0100

    secondaparte

--- main.js
+++ main.js
@@ -8,3 +8,31 @@ window.addEventListener('scroll', function () {
     }
 });
 });
+
+
+
+//Contatore visite
+
+
+function animateCounters() {
+    let counters = document.querySelectorAll('.count'); 
+  
+    counters.forEach(counter => {
+      let target = +counter.getAttribute('data-target'); 
+      let increment = target / 200; 
+  
+      let updateCounter = () => {
+        let currentValue = +counter.innerText; 
+        if (currentValue < target) {
+          counter.innerText = Math.ceil(currentValue + increment); 
+          setTimeout(updateCounter, 10); 
+        } else {
+          counter.innerText = target; 
+        }
+      };
+  
+      updateCounter();
+    });
+  }
+  
+  window.addEventListener('load', animateCounters);
